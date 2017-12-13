6a84a8f992465c40c1c5a5a92c0f02ce9dc1300a
Vitor C. Piro <vitorpiro@gmail.com>
2017-12-13 15:57:28 +0100

    gzip input and output support for file writer

--- split_fna_bins.sh
+++ split_fna_bins.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-# Usage: file.fasta.gz taxsbpbins.txt outputfolder/
+# Usage: "file.fasta[.gz] [file2.fasta[.gz] ...]" bins.txt outputfolder/ [gzip]
 
-# No prints/no gz
-#awk -v taxsbpfile="../arc_bac_all_ar/2017-09-25_18-28-39_b59.txt" '
-#BEGIN{while (getline < taxsbpfile){clust[$1]=$4;}; close(taxsbpfile);}
-#/^>/ {filen=clust[substr($1,2)]}
-#{print $0 > filen".fna"}' ../arc_bac_all_20170925.fna
-
-zcat $1 | 
-awk -v taxsbpfile=$2 -v outputfolder=$3 '
+zcat --force $1 | 
+awk -v taxsbpfile=$2 -v outputfolder=$3 -v gzipcat=$(if [ $4 == "gzip" ]; then echo "gzip"; else echo "cat"; fi) -v ext=$(if [ $4 == "gzip" ]; then echo ".gz"; fi) '
 BEGIN{
 	print "Parsing "taxsbpfile"...";
 	while (getline < taxsbpfile){clust[$1]=$4;}; 
@@ -23,6 +17,6 @@ filen=(acc in clust)?clust[acc]:"no_cluster";
 cont+=1;
 if(cont % int((l/1000)+1) == 0) {print cont" sequences out of "l" (" (cont/l)*100 "%)"};
 }
-{print $0 | " gzip >> "outputfolder"/"filen".fna.gz"}
+{print $0 | gzipcat " >> "outputfolder"/"filen".fna"ext}
 END{print "Done!"}' 
 
