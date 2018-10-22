d22055628fc679f0707464468f6843cf985871ad
Vitor C. Piro <vitorpiro@gmail.com>
2018-10-22 19:57:02 +0200

    fragment input first implementation

--- taxsbp/Cluster.py
+++ taxsbp/Cluster.py
@@ -2,9 +2,6 @@ class Cluster:
 	def __init__(self, seqid=None, seqlen=None):
 		self.seqlen = {seqid: seqlen} if seqid and seqlen else {}
 
-	def update_cluster(self,cluster):
-		self.seqlen.update(cluster.get_seqlen())
-
 	def get_seqlen(self):
 		return self.seqlen
 
@@ -13,6 +10,12 @@ class Cluster:
 
 	def get_ids(self):
 		return self.seqlen.keys()
+
+	def add(self, seqid, seqlen):
+		self.seqlen[seqid] = seqlen
+
+	def update(self,cluster):
+		self.seqlen.update(cluster.get_seqlen())
 
 	def __repr__(self):
 		args = ['{}={}'.format(k, repr(v)) for (k,v) in vars(self).items()]
