6f7b828849d00062dc6bdd2398b6108143dcc489
Vitor C. Piro <vitorpiro@gmail.com>
2019-01-28 15:33:12 +0100

    get assembly accession in batches

--- scripts/get_len_taxid.sh
+++ scripts/get_len_taxid.sh
@@ -8,6 +8,11 @@ then
 	api_key="&api_key=${NCBI_API_KEY}"
 fi
 
+if [ ! -z "${GET_ASSEMBLY_ACCESSION}" ]
+then
+	assembly_accession="true"
+fi
+
 retrieve_summary_xml()
 {
 	echo "$(curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=nuccore&id=${1}${2}")"
@@ -16,6 +21,17 @@ retrieve_summary_xml()
 retrieve_nucleotide_fasta_xml()
 {
 	echo "$(curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&rettype=fasta&retmode=xml&id=${1}${2}")"
+}
+
+retrieve_assembly_uid_xml()
+{
+	#echo "$(curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=assembly&term=${1}${2}")"
+	echo "$(curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=nuccore&db=assembly&id=${1}${2}")"
+}
+
+retrieve_assembly_accession_xml()
+{
+	echo "$(curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=assembly&id=${1}${2}")"
 }
 
 get_lines()
@@ -27,6 +43,7 @@ batch_count=1
 acc="$(get_lines ${1} 1 ${batch})"
 while [[ ! -z "${acc}" ]];
 do
+	out=""
 
 	for i in $(seq 1 ${att});
 	do
@@ -34,19 +51,16 @@ do
 		# replacing \n for , 
 		xml_summary="$(retrieve_summary_xml "${acc//$'\n'/,}" "${api_key}")"
 		acc_summary="$(echo "${xml_summary}" | grep -oP '(?<=Name="AccessionVersion" Type="String">)[^<]+')"
-
 		# if no accession returned, try again
 		if [[ -z "${acc_summary}" ]]; then 
 			continue
 		else
 			len_summary="$(echo "${xml_summary}" | grep -oP '(?<=Name="Length" Type="Integer">)[^<]+')"
 			taxid_summary="$(echo "${xml_summary}" | grep -oP '(?<=Name="TaxId" Type="Integer">)[^<]+')"
-			out_summary="$(paste <(echo "$acc_summary") <(echo "$len_summary") <(echo "$taxid_summary") --delimiters '\t')"
-			echo "${out_summary}"
+			out="$(paste <(echo "${acc_summary}") <(echo "${len_summary}") <(echo "${taxid_summary}") --delimiters '\t')"
 			break
 		fi
 	done
-
 	# if there are less output lines than input accessions, get accessions difference (or return is empty)
 	if [[ "$(echo "${acc_summary}" | wc -l)" -lt "$(echo "${acc}" | wc -l)" ||  -z "${acc_summary}"  ]];
 	then
@@ -68,8 +82,8 @@ do
 			else
 				len_fetch="$(echo "${xml_fetch}" | grep -oP '(?<=<TSeq_length>)[^<]+')"
 				taxid_fetch="$(echo "${xml_fetch}" | grep -oP '(?<=<TSeq_taxid>)[^<]+')"
-				out_fetch="$(paste <(echo "$acc_fetch") <(echo "$len_fetch") <(echo "$taxid_fetch") --delimiters '\t')"
-				echo "${out_fetch}"
+				out_fetch="$(paste <(echo "${acc_fetch}") <(echo "${len_fetch}") <(echo "${taxid_fetch}") --delimiters '\t')"
+				out="${out}"$'\n'"${out_fetch}"
 				break
 			fi
 		done
@@ -81,6 +95,87 @@ do
 		fi
 	fi
 
+	# if should retrieve assembly accessions
+	if [[ -z "${assembly_accession}" ]]; 
+	then
+		echo "${out}"
+	else
+		# Get assembly uid
+		acc_retrieved="$(echo "${out}" | cut -f 1)"
+
+		uid_link=""
+		acc_link=""
+		acc_uid_link=""
+		if [[ ! -z "${acc_retrieved}" ]]; then 
+			for i in $(seq 1 ${att});
+			do
+				xml_link="$(retrieve_assembly_uid_xml "${acc_retrieved//$'\n'/&id=}" "${api_key}")"
+				# request with several &id= instead of comma separated to get in order
+				all_id_link="$(echo "${xml_link}" | tr -d '\n' | grep -oP '(?<=<LinkSet>).*?(?=</LinkSet>)' | tr -d ' ' | tr -d '\t')"
+				
+				# link accession with link containing id, remove containing errors, just pass containing desired tag
+				# assuming that the results are returning on the same order of the input
+				out_link="$(paste <(echo "$acc_retrieved") <(echo "${all_id_link}") --delimiters '\t' | grep -v "ERROR" | grep "</Id></Link></LinkSetDb>" )"
+
+				# get accessions that were retrieved
+				acc_link="$(echo "${out_link}" | cut -f 1)"
+				
+				# if no uids were retrieved
+				if [[ -z "${acc_link}" ]]; then 
+					continue
+				else
+					# Get only last Id inside LinkSetDb (can have several assemblies)
+					uid_link="$(echo "${out_link}" | cut -f 2 | grep --color -oP '(?<=<Id>)[0-9]*?(?=</Id></Link></LinkSetDb>)')"
+					acc_uid_link="$(paste <(echo "${acc_link}") <(echo "${uid_link}") --delimiters '\t')"
+					break
+				fi
+			done
+
+
+			# if there are less output lines than input accessions, get accessions missing (or return is empty)
+			if [[ "$(echo "${acc_link}" | wc -l)" -lt "$(echo "${acc_retrieved}" | wc -l)" || -z "${acc_link}" ]];
+			then
+				acc_missing="$(diff --changed-group-format='%>' --unchanged-group-format='' <(echo "${acc_link}") <(echo "$acc_retrieved"))"
+				failed_assembly="${failed_assembly}${acc_missing}\n"
+			fi
+		fi
+
+		# if managed to get uid for assembly
+		uid_summary_assembly=""
+		assemblyaccession_summary_assembly=""
+		uid_assemblyaccession_summary_assembly=""
+		if [[ ! -z "${uid_link}" ]]; then 
+
+			for i in $(seq 1 ${att});
+			do
+				# Retrieve assembly accessions
+				xml_summary_assembly="$(retrieve_assembly_accession_xml "${uid_link//$'\n'/,}" "${api_key}")"
+				uid_summary_assembly="$(echo "${xml_summary_assembly}" | grep -oP '(?<=DocumentSummary uid=\")[^\"]+')"
+				if [[ -z "${uid_summary_assembly}" ]]; then 
+					continue
+				else
+					assemblyaccession_summary_assembly="$(echo "${xml_summary_assembly}" | grep -oP '(?<=<AssemblyAccession>)[^<]+')"
+					uid_assemblyaccession_summary_assembly="$(paste <(echo "${uid_summary_assembly}") <(echo "${assemblyaccession_summary_assembly}") --delimiters '\t')"
+					break
+				fi
+			done
+			# if there are less output lines than input accessions, get accessions missing (or return is empty)
+			if [[ "$(echo "${uid_summary_assembly}" | wc -l)" -lt "$(echo "${uid_link}" | wc -l)" ||  -z "${uid_summary_assembly}" ]];
+			then
+				echo "here 2"
+				uid_missing="$(diff --changed-group-format='%>' --unchanged-group-format='' <(echo "${uid_summary_assembly}") <(echo "$uid_link"))"
+				acc_missing="$(join -1 1 -2 2 <(echo "${uid_missing}" | sort | uniq) <(echo "${acc_uid_link}" | sort -k 2,2) -t$'\t' -o "2.1")"
+				failed_assembly="${failed_assembly}${acc_missing}\n"
+			fi
+
+		fi
+
+		# link uids (not found for esummary)
+		acc_assembly="$(join -1 2 -2 1 <(echo "${acc_uid_link}" | sort -k 2,2) <(echo "${uid_assemblyaccession_summary_assembly}" | sort -k 1,1 | uniq) -t$'\t' -o "1.1,2.2" -a 1 -e NOT_FOUND)"
+		# print final (not found for elink)
+		echo "$(join -1 1 -2 1 <(echo "${out}" | sort -k 1,1) <(echo "${acc_assembly}" | sort -k 1,1) -t$'\t' -o "1.1,1.2,1.3,2.2" -a 1 -e NOT_FOUND)"
+	fi
+
 	# read new batch
 	batch_count=$((batch_count+batch))
 	acc="$(get_lines ${1} ${batch_count} ${batch})"
@@ -90,6 +185,14 @@ done
 if [[ ! -z "${failed}" ]];
 then
 	(>&2 printf "Failed to get taxid and sequence length:\n${failed}")
-	exit 1
 fi
-exit 0
+if [[ ! -z "${failed_assembly}" ]];
+then
+	(>&2 printf "Failed to get assembly accession:\n${failed_assembly}")
+fi
+if [[ ! -z "${failed}" || ! -z "${failed_assembly}" ]];
+then
+	exit 1
+else
+	exit 0
+fi
