93a6e9646aa86430b6fe5caef1f07e14cb756a78
Vitor C. Piro <pirovc@posteo.net>
2020-07-16 13:31:45 +0200

    bump dev version 1.0.0

--- taxsbp/TaxSBP.py
+++ taxsbp/TaxSBP.py
@@ -52,7 +52,7 @@ def main():
 	cluster_parser.add_argument('-r', metavar='<bin_exclusive>', dest="bin_exclusive", type=str, default="", help="Make bins rank/taxid/specialization exclusive, so bins won't have mixed sequences. When the chosen rank is not present on a sequence lineage, this sequence will be taxid/specialization exclusive. [none,specialization name,taxid,species,genus,...] Default: none")
 	cluster_parser.add_argument('-z', metavar='<specialization>', dest="specialization", type=str, default="", help="Specialization name (e.g. assembly, strain). If given, TaxSBP will cluster entries on a specialized level after the taxonomic id. The specialization identifier should be provided as an extra collumn in the input_file ans should respect the taxonomic hiercharchy (one taxid -> multiple specializations / one specialization -> one taxid). Default: ''")
 	cluster_parser.add_argument('-u', metavar='<update_file>', dest="update_file", type=str, default="", help="Previously generated files to be updated. Default: ''")
-	cluster_parser.add_argument('-v', action='version', version='%(prog)s 0.1.2')
+	cluster_parser.add_argument('-v', action='version', version='%(prog)s 1.0.0')
 
 	if len(sys.argv)==1: # Print help calling script without parameters
 		cluster_parser.print_help() 
